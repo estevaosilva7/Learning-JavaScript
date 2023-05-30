1b1d3b32476b43e57d115105e91f88e6ac4f755b
estevaosilva7 <estevaosilva.sg@gmail.com>
2023-05-30 16:43:34 -0300

    Particularidades do JavaScript

---
+++ particularidades_js/scripts.js
@@ -0,0 +1,21 @@
+console.log("Primeiro Script");
+
+console.log("Último Script");
+
+console.log("Case Sensitive");
+
+var a = 7;
+
+a = "Teste de tipagem fraca";
+
+console.log(a);
+
+// Comemtários de uma linha!
+
+/* Mais
+De
+Uma 
+Linha 
+Sendo
+Comentada!
+*/
