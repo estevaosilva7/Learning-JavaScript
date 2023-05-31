acc76285aa14e04ec6be617b8fb5b0226752efda
estevaosilva7 <estevaosilva.sg@gmail.com>
2023-05-31 16:24:27 -0300

    Variáveis

---
+++ variaveis/scripts.js
@@ -0,0 +1,38 @@
+var teste = 7;
+
+console.log(teste);
+
+teste = "Estevão";
+
+console.log(teste);
+
+// var @nome não funciona 
+// var começando com Número não funciona, mas depois de letra sim
+
+var $nome = "ADS";
+
+var _nome = "IFS"
+
+console.log($nome);
+console.log(_nome);
+
+idade = 17 // Variável global sem o *var*
+console.log(idade);
+
+var nomeCurso = "Engenharia de Software"; // Padrão camelCase
+
+console.log(nomeCurso);
+
+let ola = "Oi";
+const oi = "Olá";
+
+console.log(oi)
+console.log(ola)
+
+var CPF;
+
+console.log(CPF);
+
+CPF = 11122233345;
+
+console.log(CPF);
