2ac110a899184f54d78d3e65e2b3888238fd81a7
estevaosilva7 <estevaosilva.sg@gmail.com>
2023-05-31 18:45:48 -0300

    Tipos de Dados

---
+++ tipos_dados/scripts.js
@@ -0,0 +1,25 @@
+var numero = 24;
+
+console.log(numero);
+console.log( typeof numero);
+
+var float = 3.14;
+
+console.log(float);
+console.log(typeof float);
+
+var textoComNumero = "777";
+
+console.log(textoComNumero);
+console.log(typeof textoComNumero);
+
+console.log(NaN);
+console.log(typeof NaN);
+console.log(typeof +Infinity);
+console.log(typeof -Infinity);
+
+teste1 = numero + textoComNumero;
+console.log(teste1);
+
+teste2 = float + textoComNumero;
+console.log(teste2);
