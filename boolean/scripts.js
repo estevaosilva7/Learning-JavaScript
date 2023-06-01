d922d95871a9ec4db567152db0fd68f07c56e79b
estevaosilva7 <estevaosilva.sg@gmail.com>
2023-06-01 16:54:51 -0300

    String

---
+++ boolean/scripts.js
@@ -0,0 +1,8 @@
+var verdadeiro = true;
+var falso = false;
+
+console.log(verdadeiro);
+console.log(falso);
+
+console.log(typeof verdadeiro);
+console.log(typeof falso);
---
+++ strings/scripts.js
@@ -0,0 +1,14 @@
+var nome  = "Estevão";
+var sobreNome = "Silva";
+
+console.log(nome);
+console.log(typeof nome);
+
+var frase = "Mas que nome bonito";
+
+nomeCompleto = nome + " " + sobreNome;
+
+console.log(nomeCompleto);
+
+
+document.write('Esse é aqui é meu documento HTML sendo escrito pelo JavaScript! "Iaeeee"');
