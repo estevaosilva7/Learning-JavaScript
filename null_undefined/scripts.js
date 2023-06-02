892f01299433971a04514238dc0788e829b0a4d1
estevaosilva7 <estevaosilva.sg@gmail.com>
2023-06-02 16:34:01 -0300

    Null e Undefined

---
+++ null_undefined/scripts.js
@@ -0,0 +1,14 @@
+// Hoisting - içamento
+
+var nome = null;
+var sobrenome;
+
+console.log(nome);
+console.log(sobrenome);
+
+var nome = "Estevão";
+var sobrenome = "Silva";
+
+console.log(nome);
+console.log(sobrenome);
+
