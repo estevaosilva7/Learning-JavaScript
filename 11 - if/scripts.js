28c3119876e8e9f3976b90e29622cdf70b452e1e
estevaosilva7 <estevaosilva.sg@gmail.com>
2023-06-04 20:20:06 -0300

    If

---
+++ 11 - if/scripts.js
@@ -0,0 +1,13 @@
+var idadeMinima = 18;
+
+console.log("Antes do if");
+
+if(idadeMinima => 19) {
+    console.log("Pode tirar habilitação")
+}
+
+if(true) {
+    console.log("Testando o IF")
+};
+
+console.log("Depois do if")
