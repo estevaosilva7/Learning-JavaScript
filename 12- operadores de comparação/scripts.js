fcc7f0abc8ecdff8cd1a567071916344b784ce3f
estevaosilva7 <estevaosilva.sg@gmail.com>
2023-06-04 20:33:35 -0300

    Operadores de comparação

---
+++ 12- operadores de comparação/scripts.js
@@ -0,0 +1,21 @@
+var idade= 24;
+
+
+if(idade >= 18) {
+    console.log("Pode tirar habilitação")
+}
+
+ if(idade <= 17) {
+    console.log("Você não pode tirar habilitação")
+ }
+
+var nome = "Carlos";
+
+if(nome == "Estevão") {
+    console.log("Que cara bonito")
+}
+
+if(nome != "Estevão") {
+    console.log("Se não for o Estevão é feio")
+}
+ 
