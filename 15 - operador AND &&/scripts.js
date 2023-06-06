ac7ba0efa134aa089a07bcefa4a0661480f9bae8
estevaosilva7 <estevaosilva.sg@gmail.com>
2023-06-06 16:17:19 -0300

    Operador And

---
+++ 15 - operador AND &&/scripts.js
@@ -0,0 +1,17 @@
+var nome = "Estevão";
+var idade = 7;
+
+if(nome == "Estevão" && idade == 7) {
+    console.log("Pode acessar o computador");
+} else {
+    console.log("Acesso negado");
+}
+
+var nome = "Estevão";
+var idade = 8;
+
+if(nome == "Estevão" && idade == 7) {
+    console.log("Pode acessar o computador");
+} else {
+    console.log("Acesso negado");
+}
