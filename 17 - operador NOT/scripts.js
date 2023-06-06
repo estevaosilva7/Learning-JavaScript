d7e86162c7c253aca397e517d72371e587e706d6
estevaosilva7 <estevaosilva.sg@gmail.com>
2023-06-06 16:32:36 -0300

    Operador NOT

---
+++ 17 - operador NOT/scripts.js
@@ -0,0 +1,13 @@
+var nome = "ADS";
+
+if(nome == "ADS") {
+    console.log("Curso de TI");
+} else {
+    console.log("Não é de TI")
+}
+
+if(!(nome == "ADS")) {
+    console.log("Curso de TI");
+} else {
+    console.log("Não é de TI")
+}
