f2e16e3ad59a4e5de18ed66cfcd1e02991858488
estevaosilva7 <estevaosilva.sg@gmail.com>
2023-06-05 22:54:46 -0300

    Comparação tipo de dado e valor

---
+++ 14 - Comparação tipo de dado e valor/scripts.js
@@ -0,0 +1,15 @@
+// == Ver se o dado é igual
+// === Ver se o tipo de dado é igual
+// !== Ver a diferença do tipo de dado
+
+var numero = 1;
+
+if(numero === 7) {
+    console.log("Esse é o número 7 do tipo number");
+} 
+else if(numero === "7") 
+{   console.log("Esse número 7 é do tipo string");
+}
+else(numero !== 7) 
+{   console.log("Esse não é o 7 de nenhum tipo")
+}
