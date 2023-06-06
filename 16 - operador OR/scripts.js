b3ea20b46f96acb2c6d7a1ab72dd7da69d6ed52f
estevaosilva7 <estevaosilva.sg@gmail.com>
2023-06-06 16:26:26 -0300

    Operdaor OR

---
+++ 16 - operador OR/scripts.js
@@ -0,0 +1,26 @@
+var nome = "Estevão";
+var idade = 7;
+
+if(nome == "Estevão" || idade == 7) {
+    console.log("Pode acessar o computador");
+} else {
+    console.log("Acesso negado");
+}
+
+var nome = "Estevão";
+var idade = 8;
+
+if(nome == "Estevão" || idade == 7) {
+    console.log("Pode acessar o computador");
+} else {
+    console.log("Acesso negado");
+}
+
+var nome = "Carlos";
+var idade = 8;
+
+if(nome == "Estevão" || idade == 7) {
+    console.log("Pode acessar o computador");
+} else {
+    console.log("Acesso negado");
+}
