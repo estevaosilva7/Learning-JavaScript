9ae33ba7bde262d0bdc0fcc765ec5921a87f6edb
estevaosilva7 <estevaosilva.sg@gmail.com>
2023-06-05 21:58:25 -0300

    Else if e else

---
+++ Else if e else/scripts.js
@@ -0,0 +1,29 @@
+var nome = "Carlos";
+
+if(nome == "Estevão") {
+    console.log("O nome é Estevão");
+} 
+
+else if (nome == "Carlos") {
+    console.log("O nome é Carlos");
+}
+else {
+    console.log("Nome diferente");
+}
+
+var idade = 17;
+
+if(idade < 18) {
+    console.log("Não pode entrar");
+}
+else{
+    console.log("Pode entrar");
+}
+
+var nota = 0;
+
+if(nota == 1) {
+    console.log("Aprovado");
+} else if(nota == 0) {
+    console.log("Reprovado");
+}
