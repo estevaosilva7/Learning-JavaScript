0b94429df603e782301ef5acd6ed037aad5dc2f0
estevaosilva7 <estevaosilva.sg@gmail.com>
2023-06-09 16:09:26 -0300

    Function

---
+++ 22 - function/scripts.js
@@ -0,0 +1,27 @@
+function primeiraFuncao() {
+    console.log("Hello Word das Funções");
+}
+
+primeiraFuncao();
+
+function dizerNome(nome) {
+    console.log("Meu nome é " + nome);
+}
+
+dizerNome("Estevão");
+dizerNome("Carlos");
+
+var bancoDados = "Hall";
+dizerNome(bancoDados);
+
+function soma(a, b) {
+    var soma = a + b;
+    return soma;
+    console.log(soma);
+}
+
+var soma1 = soma(7, 7);
+console.log(soma1);
+
+var soma2 = soma(21,56);
+console.log(soma2);
