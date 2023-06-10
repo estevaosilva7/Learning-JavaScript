8acb0331a10650e3f26ad851304030f55bbe9a74
estevaosilva7 <estevaosilva.sg@gmail.com>
2023-06-09 21:24:21 -0300

    Métodos Numéricos

---
+++ 26 - metodos_numericos/scripts.js
@@ -0,0 +1,24 @@
+// parseFloat 10.0
+
+console.log(parseFloat("17.999"));
+console.log(Number.parseFloat("17.999"));
+
+//parseInt
+
+console.log(parseInt("7"));
+console.log(parseInt("17.777"));
+
+// toFixed
+
+console.log(27.51515151.toFixed(1));
+
+//isNaN
+
+console.log(isNaN("teste"));
+console.log(isNaN(71));
+console.log(isNaN("14"));
+
+// MAX_VALUE e MIN_VALUE
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
