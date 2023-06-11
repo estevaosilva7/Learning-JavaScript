a633e541fbbdda9d7f165d9564926d478b6b3a4c
estevaosilva7 <estevaosilva.sg@gmail.com>
2023-06-11 19:03:43 -0300

    Métodos Array 2

---
+++ 30 - metodos_array 2/scripts.js
@@ -0,0 +1,24 @@
+// splice 
+
+var array = [1,2,3,4,5,];
+console.log(array);
+
+array.splice(2,0,777);
+console.log(array);
+
+array.splice(4,1);
+console.log(array);
+
+// indexof
+
+console.log(array.indexOf(5));
+
+// join
+
+var array2 = ["O", "rato", "roeu", "a", "roupa"];
+console.log(array2.join(" "));
+console.log(array2.join(","));
+
+//reverse
+
+console.log(array2.reverse());
