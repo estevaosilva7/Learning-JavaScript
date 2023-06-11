ca30eaa01b2ee55aa6809cfc7f182168f0e76501
estevaosilva7 <estevaosilva.sg@gmail.com>
2023-06-11 20:08:29 -0300

    Acessando DOM

---
+++ 33 - acessando_dom/scripts.js
@@ -0,0 +1,19 @@
+// tag
+
+var titulo = document.getElementsByTagName("h1")[0];
+console.log(titulo);
+
+var lista = document.getElementsByTagName("li");
+console.log(lista)
+
+// id
+
+var paragrafo = document.getElementById("paragrafo");
+console.log(paragrafo);
+
+//class
+
+var itemlista = document.getElementsByClassName("item");
+console.log(itemlista);
+
+
