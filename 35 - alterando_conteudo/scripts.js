12441f0e072703e8fce39eb74e10d132c5be7719
estevaosilva7 <estevaosilva.sg@gmail.com>
2023-06-13 17:37:55 -0300

    Alterando Conteúdo

---
+++ 35 - alterando_conteudo/scripts.js
@@ -0,0 +1,15 @@
+// Selecionar elemento
+
+var title = document.querySelector("#title");
+console.log(title);
+
+// innerHTML
+
+title.innerHTML ="Testando alteração";
+
+//textContent => mais utilizado, recomenado e peformático
+
+var subtiltle = document.querySelector(".subtitle");
+console.log(subtiltle);
+
+subtiltle.textContent ="Textando o Context";
