cd5e30b018990f2f9caa9f4833be9bb807a1bfd4
estevaosilva7 <estevaosilva.sg@gmail.com>
2023-06-12 21:13:23 -0300

    querySelector

---
+++ 34 - querySelector/scripts.js
@@ -0,0 +1,21 @@
+var itemClasse = document.getElementsByClassName("item");
+console.log(itemClasse);
+
+// querySelectorAll
+var itemQuery = document.querySelectorAll("#lista2 li");
+console.log(itemQuery);
+
+var itemQuery2 = document.querySelectorAll("#lista .item");
+console.log(itemQuery2);
+
+//querySellector
+
+var lista = document.querySelector("#lista");
+console.log(lista);
+
+var primeiraLista = document.querySelector("ul");
+console.log(primeiraLista);
+
+var span = document.querySelector("#paragrafo span");
+console.log(span);
+
