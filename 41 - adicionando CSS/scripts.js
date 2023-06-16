24724e8eae8776baddef4d80aa433780cb252f0b
estevaosilva7 <estevaosilva.sg@gmail.com>
2023-06-16 16:33:59 -0300

    Adicionando CSS

---
+++ 41 - adicionando CSS/scripts.js
@@ -0,0 +1,15 @@
+// selecionando elemento
+var title = document.querySelector("#title");
+
+//adicionando estilo
+title.style.color ="red";
+
+//background-color
+title.style.backgroundColor ="blue";
+
+// selecionando elemento
+var subtitle = document.querySelector(".subtitle");
+subtitle.style.color = "purple";
+
+//adicionando vários estilo
+subtitle.style.cssText = "color: red; background-color: black; font-size: 50px";
